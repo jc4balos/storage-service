c9011c492f712df9a3203687aa14631c1fa7dc6d
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-11-24 08:17:39 -0300

    Create model layer

---
+++ src/main/java/com/example/storage/service/model/File.java
@@ -0,0 +1,28 @@
+package com.example.storage.service.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private String filePath;
+
+}
