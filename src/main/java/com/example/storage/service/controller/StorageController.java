06548aa9deba1c6fb0a3418f5936205247c18ec9
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-11-24 08:52:01 -0300

    Create controller layer

---
+++ src/main/java/com/example/storage/service/controller/StorageController.java
@@ -0,0 +1,39 @@
+package com.example.storage.service.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.storage.service.service.StorageService;
+
+@RestController
+public class StorageController {
+
+    @PostMapping("/image")
+    public ResponseEntity<?> uploadImageToDatabase(@RequestParam("image") MultipartFile multipartFile)
+            throws IOException {
+        String image = storageService.uploadImageToDatabase(multipartFile);
+
+        return ResponseEntity.status(HttpStatus.OK).body(image);
+    }
+
+    @GetMapping("/image/{filename}")
+    public ResponseEntity<?> downloadImageFromDatabase(@PathVariable(name = "filename") String fileName) {
+        byte[] image = storageService.downloadImageFromDatabase(fileName);
+
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png")).body(image);
+    }
+
+    @Autowired
+    private StorageService storageService;
+
+}
